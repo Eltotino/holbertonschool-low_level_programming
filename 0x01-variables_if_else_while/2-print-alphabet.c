f038b8c0dc7a57c452675a198117c558f40bd2d9
Eltotino <totojaber1@gmail.com>
2020-02-06 13:21:46 +0100

    alphabet

---
+++ 0x01-variables_if_else_while/2-print-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * i made it
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char bb = 'a';
+
+	while (bb <= 'z')
+	{
+	putchar(bb);
+	bb++;
+			}
+	putchar('\n');
+	return (0);
+}
