6caade2d2034f7a53cb6c60ba1fa674cf4a8ee00
Eltotino <totojaber1@gmail.com>
2020-02-06 14:10:47 +0100

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char bb;
+for (bb = 'a'; bb <= 'z'; ++bb)
+{
+	if (bb != 'e' && bb != 'q')
+putchar(bb);
+}
+
+putchar('\n');
+	return (0);
+}
