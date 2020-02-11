825d18fe6dbcc86617db940d9f7925744bc9df44
Eltotino <totojaber1@gmail.com>
2020-02-11 12:29:47 +0100

    alpahabet

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* main - print Holberton
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+int i;
+char ch[] = "Holberton";
+
+for (i = 0; i < 9; i++)
+
+_putchar(ch[i]);
+
+_putchar('\n');
+
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* print_alphabet - print alphabet in lower case
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+
+char tt;
+
+for (tt = 'a'; tt <= 'z'; tt++)
+
+_putchar(tt);
+
+
+
+_putchar('\n');
+
+return;
+}
