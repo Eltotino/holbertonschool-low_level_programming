fcc3f6c5ff8c40afa3ff24249e2c95e2c8c2833c
Eltotino <totojaber1@gmail.com>
2020-02-11 18:19:22 +0100

    adds

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * add - add an integer to an other
+ * @b: integer
+ * @a:integer
+ * Return:  a+ b
+ */
+int add(int b, int a)
+{
+return (a + b);
+}
