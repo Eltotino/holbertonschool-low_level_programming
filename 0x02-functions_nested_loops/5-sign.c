fdf2771e62396167f48022a8e45878a82c288a99
Eltotino <totojaber1@gmail.com>
2020-02-11 14:30:31 +0100

    printsign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* print_sign - checks for signs charachters
+*
+* Return: Always 0 (Success)
+*@n: integer
+*/
+int print_sign(int n)
+{
+
+if (n > 0)
+{
+	_putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+	_putchar (48);
+	return (0);
+}
+else
+	{
+		_putchar (45);
+		return (-1);
+	}
+
+}
