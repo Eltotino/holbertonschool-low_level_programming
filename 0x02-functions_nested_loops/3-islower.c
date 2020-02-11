7db30aca1d8b461b9b0ea007cc4af3c34bc4c7a3
Eltotino <totojaber1@gmail.com>
2020-02-11 13:42:42 +0100

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* _islower - check for lower case characters
+*
+* Return: Always 0 (Success)
+*@c: integer
+*/
+int _islower(int c)
+{
+
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+
+{	return (0);
+}
+
+
+}
