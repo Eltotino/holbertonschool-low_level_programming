d31b82807294965249d6d567fa5f10976b0d10b1
Eltotino <totojaber1@gmail.com>
2020-02-11 14:56:27 +0100

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _abs - checks for absolute value
+*
+* Return: Always 0 (Success)
+*@n: integer
+*/
+int _abs(int n)
+{
+
+if (n >= 0)
+{
+
+return (n);
+}
+return (-n);
+
+}
