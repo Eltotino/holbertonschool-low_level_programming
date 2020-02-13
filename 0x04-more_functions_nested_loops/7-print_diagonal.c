45770ec13aa0b83b4c2070edadae2aa11932d712
Eltotino <totojaber1@gmail.com>
2020-02-13 15:21:20 +0100

    update

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print diagonals
+ *
+ *@n: integer
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int column, row;
+	
+if (n <= 0)
+{_putchar('\n');
+}
+
+else
+
+	for (row = 0; row < n; row++)
+
+	{
+		for (column = 0; column <= row; column++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar('\n');
+}
+}
