fe08ad010f4057785066a6a8e4149ed15502c3d1
Eltotino <totojaber1@gmail.com>
2020-02-13 15:32:45 +0100

    update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,20 +9,21 @@
 void print_diagonal(int n)
 {
 	int column, row;
-if (n <= 0)
-{_putchar('\n');
-}
 
-else
-
-	for (row = 0; row < n; row++)
-
+	if (n <= 0)
 	{
-		for (column = 0; column <= row; column++)
-		{
-			_putchar(' ');
-		}
-		_putchar(92);
 		_putchar('\n');
-}
+	}
+	else
+	{
+		for (row = 0; row < n; row++)
+		{
+			for (column = 0; column <= row - 1; column++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
 }
