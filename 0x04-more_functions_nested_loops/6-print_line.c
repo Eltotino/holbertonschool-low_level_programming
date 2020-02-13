5feaa7461ee09018a7fb11f908fa2c5aa3a1d948
Eltotino <totojaber1@gmail.com>
2020-02-13 13:37:45 +0100

    more nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* print_line - prints lines
+*
+* Return: Always 0 (Success)
+*@n: integer
+*/
+void print_line(int n)
+{
+int b;
+
+	if (n > 0)
+
+
+	{
+		for (b = 0; b <= n - 1; b++)
+			{_putchar('_');
+	}
+		_putchar('\n');
+	
+	}
+	_putchar('\n');
+}
