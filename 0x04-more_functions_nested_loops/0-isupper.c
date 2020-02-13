392c2b0a574b8260f574bca84e1ca0a3e98f7d92
Eltotino <totojaber1@gmail.com>
2020-02-13 10:55:35 +0100

    more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* isupper - check for upper case characters
+*
+* Return: Always 0 (Success)
+*@c: integer
+*/
+int _islower(int c)
+{
+
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+
+{	return (0);
+}
+
+
+}
