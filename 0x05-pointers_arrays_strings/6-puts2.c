b1b33b850c0968754c8e9f0e8a4c282813b22283
Eltotino <totojaber1@gmail.com>
2020-02-18 14:46:40 +0100

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,11 @@ void puts2(char *str)
 {
 	int i, k;
 
-	for (i = 0; str[i + 1] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		;
 	for (k = 0; k <= i - 1; k = k + 2)
-
+{
 	_putchar(str[k]);
 	_putchar('\n');
+}
 }
