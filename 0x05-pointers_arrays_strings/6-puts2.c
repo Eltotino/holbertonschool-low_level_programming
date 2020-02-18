a9a593e9bfa87fe4957dfde361199004f8dbc0bd
Eltotino <totojaber1@gmail.com>
2020-02-18 14:27:50 +0100

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* puts2 - prints every other character
+*
+*@str :pointer
+*
+* Return : string length
+*/
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i = i + 2)
+
+	{_putchar(str[i]);
+	}
+		_putchar('\n');
+}
