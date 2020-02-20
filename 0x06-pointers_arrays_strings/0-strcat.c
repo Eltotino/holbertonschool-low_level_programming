431de7c1824066f048a31bcbe97b50db614aa1ca
Eltotino <totojaber1@gmail.com>
2020-02-20 12:43:54 +0100

    more pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,14 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+int i, j;
 
-	for (i = 0; dest[i] != '\0'; ++i, j++)
+for (i = 0; dest[i] != '\0'; ++i, j++)
 ;
 for (j = 0; src[j] != '\0'; j++, i++)
 {
 
-	dest[i] = src[j];
+dest[i] = src[j];
 }
 dest[i] = '\0';
 return (dest);
