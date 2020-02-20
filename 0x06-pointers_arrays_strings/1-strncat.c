e9bad19a49327eaa4a1c3190584031bc9a5f5fd5
Eltotino <totojaber1@gmail.com>
2020-02-20 12:42:27 +0100

    more pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,15 +11,15 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	int j, i;
+int j, i;
 
-	for (i = 0; dest[i] != '\0'; ++i)
+for (i = 0; dest[i] != '\0'; ++i)
 ;
 
 for (j = 0; j < n && src[j] != '\0'; j++, i++)
 {
 
-	dest[i] = src[j];
+dest[i] = src[j];
 }
 return (dest);
 }
