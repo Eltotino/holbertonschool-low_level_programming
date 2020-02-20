e889e62b35a77991efb57910d149cd4e53974b61
Eltotino <totojaber1@gmail.com>
2020-02-20 22:10:00 +0100

    more pointers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+**leet - prints the world in leet language
+*
+*@s : pointer
+*
+*Return: leet language
+*/
+char *leet(char *s)
+{
+	int i = 0, j;
+	char a[] = {65, 97, 69, 101, 84, 116, 79, 111, 76, 108};
+	char b[] = {52, 52, 51, 51, 55, 55, 48, 48, 49, 49};
+while ( s[i] != '\0')
+	{
+for (j = 0; j < 10; j++)
+	if (s[i] == a[j])
+	{
+		s[i] = b[j];
+	}
+	i++;
+}
+	return (s);
+}
