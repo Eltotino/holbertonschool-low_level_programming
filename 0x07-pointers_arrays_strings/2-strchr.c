a364d574e1b838c61192c4b49a86089d59792417
Eltotino <totojaber1@gmail.com>
2020-02-24 20:33:41 +0100

    string char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,11 +3,10 @@
 
 
 /**
-* _memset - Fills the first @n bytes of the memory area
-*          pointed to by @s with the constant byte @c.
+* _strchr - Fonction
 * @s: A pointer to the memory area to be filled.
-* @b: The character to fill the memory area with.
-* @n: The number of bytes to be filled.
+* @c: The character to fill the memory area with.
+*
 *
 * Return: A pointer to the filled memory area @s.
 */
@@ -17,17 +16,16 @@ char *_strchr(char *s, char c)
 	int i = 0;
 	int j = 0;
 
-	while (s[i] != '\0')
+		while (s[i] != '\0')
 		{
 			i++;
 		}
 
 while (j <= i)
-{ 
+{
 	if (s[j] == c)
-	
+
 		return (&(s[j]));
-	
 	j++;
 }
 
