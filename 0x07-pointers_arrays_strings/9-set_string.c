26114f530cea6baa86ed2ba17f370860fb4ae5e6
Eltotino <totojaber1@gmail.com>
2020-02-25 22:05:58 +0100

    set string

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,17 +1,10 @@
-#include <stdio.h>
-#include "holberton.h"
+#include"holberton.h"
 /**
- * print_diagsums - Adding the diagonals of a square matrix
- * @a: The square matrix
- * @size: The size of the square matrix
- */
-void print_diagsums(int *a, int size)
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer
+ * @to: pointer
+*/
+void set_string(char **s, char *to)
 {
-int row, column, sum1 = 0, sum2 = 0;
-for (row = 0, column = 0; row < size; row++, column++)
-{
-sum1 += *(a + row * size + column);
-sum2 += *(a + row * size + (size - 1 - column));
-}
-printf("%d, %d\n", sum1, sum2);
+*s = to;
 }
