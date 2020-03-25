d4c198a070ff4933b35629e16c244954c07b61b2
Eltotino <totojaber1@gmail.com>
2020-03-25 22:04:52 +0100

    malloc

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* argstostr - function that concatenates arg of a prog 
+* @ac : int
+* @av : char
+* Return : ALways 0;
+*/
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int j;
+	int k;
+	char *s;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			k++;
+	}
+	s = malloc(sizeof(char) * k + 1 + ac);
+	k = 0;
+		for (i = 0; i < ac; i++)
+	{
+		for(j = 0; av[i][j] ; j++)
+		{
+			s[k] = av[i][j];
+			k++;
+		}
+		s[k] = '\n';
+		k++;
+	}
+	return (s);
+}
