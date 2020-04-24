bc5e494ad305729c18dfbd681291d28a38ee1b24
Eltotino <totojaber1@gmail.com>
2020-04-24 20:02:39 +0100

    print_numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+*print_numbers - print numbers
+*@separator: separate numbers
+*@n; amount of number to print 
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list lista;
+	unsigned int i;
+
+	va_start(lista, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(lista, int));
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(lista);
+}
