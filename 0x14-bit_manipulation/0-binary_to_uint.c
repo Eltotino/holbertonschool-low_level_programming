3e9d20005ffa82be73b39175d964bebde1c35047
Eltotino <totojaber1@gmail.com>
2020-04-25 21:36:37 +0100

    binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 	j = 0;
 	i = 0;
 
-		while(b[i] != '\0')
+		while (b[i] != '\0')
 		{
 			if (b[i] != '0' && b[i] != '1')
 			return (0);
@@ -25,6 +25,6 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (j);
 
-	if(b == NULL)
-		return(0);
+	if (b == NULL)
+		return (0);
 }
