bf745b90f227e49235e65f48190d2df88ff9dd14
Eltotino <totojaber1@gmail.com>
2020-04-25 21:56:18 +0100

    get_bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* get_bit - returns value of a bit
+*@n: unsigned int
+*@index: index
+*Return:value of bit
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+
+	if (index > 10)
+		return (-1);
+	else
+	bit = (n >> index) & 1;
+	return (bit);
+}
