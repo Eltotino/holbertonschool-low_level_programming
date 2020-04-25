fe94cccd79bad50f59187682e507e618df3d77fc
Eltotino <totojaber1@gmail.com>
2020-04-25 14:00:58 +0100

    advanced

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -4,11 +4,11 @@ global    main
 main:     mov       rax, 1                  
           mov       rdi, 1                  
           mov       rsi, message            
-          mov       rdx, 16                
+          mov       rdx, 17                
           syscall                           
           mov       rax, 60                
           xor       rdi, rdi                
           syscall                           
 
           section   .data
-message:  db     "Hello, Holberton",10
+message:  db  "Hello, Holberton",10
