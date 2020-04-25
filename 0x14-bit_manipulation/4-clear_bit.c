0f83c89c3987376af8cfdd43b59cc721dc710b44
Eltotino <totojaber1@gmail.com>
2020-04-25 22:21:38 +0100

    clear

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
-*clear_bit -clears a bit 
+*clear_bit -clears a bit
 *@n: pointer to int
 *@index : where to clear bit
-*Return : 1 or -1
+*Return: 1 or -1
 */
 
 int clear_bit(unsigned long int *n, unsigned int index)
