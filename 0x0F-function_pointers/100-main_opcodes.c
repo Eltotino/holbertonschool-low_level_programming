e06a00fb27b2c64b303419d65f844f86b4534a1a
Eltotino <totojaber1@gmail.com>
2020-04-25 15:55:49 +0100

    code

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,11 +8,28 @@
 
 void main(int argc, char *argv[])
 {
+	int i, n;
+	char *code;
+
 	if (argc != 2)
-		printf("Error\n");
-		exit(1);
+		{
+			printf("Error\n");
+			exit(1);
+				}
+	n = atoi(argv[1]);
 
 	if (argv[1] < 0)
-		printf("Error\n");
-		exit(2);
+		{
+			printf("Error\n");
+			exit(2);
+		}
+		code = (char *) main;
+		for (i = 0; i < n; i++)
+		{
+
+			printf("%02x", code[i] & 0xFF);
+			if(i != n -1)
+				printf(" ");
+		}
+		printf("\n");
 }
