b29b7a2bddbe6d0582b0bd026cd5d25d15b601e2
Eltotino <totojaber1@gmail.com>
2020-04-26 21:48:14 +0100

    add

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 	int i = 0;
 	list_t *new;
 
-	new = malloc(sizeof(list_t));
+	new = (list_t *) malloc(sizeof(list_t));
 
 	new->str = strdup(str);
 	new->next = *head;
