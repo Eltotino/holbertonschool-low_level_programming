ecd8665c2d8ec2d0f3beea89acaf9a31ec088bc4
Eltotino <totojaber1@gmail.com>
2020-04-26 23:24:25 +0100

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+* _no_rush - function that prints before main 
+*/
+
+
+void __attribute__((constructor)) _no_rush(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
