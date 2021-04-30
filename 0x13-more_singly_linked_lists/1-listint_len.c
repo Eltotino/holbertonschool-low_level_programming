5f4b5183a583ee2cfb035a709503ec2b296b72b9
Eltotino <totojaber1@gmail.com>
2021-04-30 16:36:54 +0100

    list_len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+*listint_len - returns lenghth of list
+*@h : pointer to list
+*Return :  length of list
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+	for (i = 0; h != NULL; i++)
+	{
+		h = h->next;
+	}
+	return i;
+}
