00505f296fc785b55e7949187edf876858262858
Eltotino <totojaber1@gmail.com>
2021-04-30 22:10:26 +0100

    free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+*free_listint - free the list
+*@head: pointer to list
+*
+*
+*Return: nothing
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *empt;
+
+	while(head != NULL)
+	{
+		empt = head;
+		head = head->next;
+		free(empt);
+	}
+}
