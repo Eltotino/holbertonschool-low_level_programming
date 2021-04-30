16110f65cfc847e69b67561f5726c8def6d31d59
Eltotino <totojaber1@gmail.com>
2021-04-30 22:28:30 +0100

    free_list2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+*free_listint2 - frees the list
+*@head: pointer to list
+*
+*
+*Return: nothing
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *empt;
+
+	while (*head != NULL)
+	{
+		empt = *head;
+		*head = (*head)->next;
+		free(empt);
+	}
+	head = NULL;
+}
