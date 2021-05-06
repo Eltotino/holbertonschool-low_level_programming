d28733ac4e55df7b52ea5a832332d07b23551690
Eltotino <totojaber1@gmail.com>
2021-05-06 12:24:27 +0100

    doubly

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*print_listint - returns number of nodes in list
+*@h: pointer to the list
+*
+* Return:  number of elements
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+return (i);
+}
