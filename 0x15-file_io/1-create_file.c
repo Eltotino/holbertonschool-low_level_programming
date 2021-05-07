256b25b5d5fffb00afb98369f9ae606ca2b561dd
Eltotino <totojaber1@gmail.com>
2021-05-07 12:20:13 +0100

    create

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <unistd.h>
+#include <fcntl.h>
+/**
+*read_textfile - deletes node at a certain index in list
+*@filename: pointer to the file
+*@letters: size of th file
+*
+* Return: size of file
+*/
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd, w;
+	int len;
+
+	if (!filename)
+			return (-1);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	if (fd < 0)
+			return (-1);
+
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+		w = write(fd, text_content, len);
+		if (w < 0)
+				return (-1);
+	}
+	return (1);
+}
