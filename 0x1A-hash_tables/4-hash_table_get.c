23230011e7b443a1abf35217e50e5771c6d81fd9
Eltotino <totojaber1@gmail.com>
2021-05-11 19:04:38 +0100

    get element

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *temp;
 	unsigned long int index;
 
+	if (!ht || !key)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
 
