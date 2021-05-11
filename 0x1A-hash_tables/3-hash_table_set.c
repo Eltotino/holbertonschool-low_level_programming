0cef0c92a3e7c0aee0f35401a41b2bbab57b17ee
Eltotino <totojaber1@gmail.com>
2021-05-11 18:46:22 +0100

    set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+*hash_table_set - insert element in hash table
+*@ht: hashtable used
+*@key: key
+*@value: element to insert
+* Return: index
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new, *temp;
+	unsigned long int index;
+
+	if (!value || !ht || !key || !*key)
+		return (0);
+	index = key_index((const unsigned char *) key, ht->size);
+	temp = ht->array[index];
+
+	while (temp)
+	{
+		if (!strcmp(key, temp->key))
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			if (!temp->value)
+				return (0);
+			return(1);
+		}
+		temp = temp->next;
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		free(new->key);
+		return (0);
+	}
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
