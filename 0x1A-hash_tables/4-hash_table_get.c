727cc8c087dd30132ee7792fb7710307692879c8
Eltotino <totojaber1@gmail.com>
2021-05-11 19:03:19 +0100

    get element

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+*hash_table_get - get element in hash table
+*@ht: hashtable used
+*@key: key
+* Return: value associated with element
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp;
+	unsigned long int index;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	while (temp)
+	{
+		if (strcmp(key, temp->key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
